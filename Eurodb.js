84e87b5e23eb17abe87af4a696cb888874e57f2b
rogelio silvan <rogoffspring@hotmail.com>
2018-11-02 09:19:07 +0100

    ejercicio testing euro2016

--- Eurodb.js
+++ Eurodb.js
@@ -38,20 +38,33 @@ class EuroDB{
         return axios.get(`https://raw.githubusercontent.com/jokecamp/FootballData/master/UEFA_European_Championship/Euro%202016/players_json/${team}-players.json`)
         .then(function(response) {
           return response.data;
-        });
+        }).catch(
+            function(response){
+                // console.error("Promesa Asíncrona numero ("+response+") - ERROR FATAL");
+                // console.log(response.response.status);
+                return response.response.status;
+          });
     }
 
     async findPlayer(ObjNamePosition, team){
+        let playerTeamFiltered = null;
+        if(!ObjNamePosition) return null;
         const playerTeam = await this.getPlayersByTeam(team);
-        // console.log(ObjNamePosition.hasOwnProperty('name'));
-        console.log(Object.keys(ObjNamePosition).toString());
-        let keyTofind = Object.keys(ObjNamePosition).toString();
-        let valueTofind = ObjNamePosition[keyTofind];
-        console.log(valueTofind);
-        console.log(players.sheets.Players.find(player => player[keyTofind] === valueTofind));
-        // console.log(playerTeam.sheets.Players)
-        return true;
+        playerTeamFiltered = playerTeam.sheets.Players.filter(player => player[Object.keys(ObjNamePosition).toString()] === ObjNamePosition[Object.keys(ObjNamePosition).toString()])
+        // console.log("Esto es: playerTeamFiltered");
+        // console.log(playerTeamFiltered.length)
+        return playerTeamFiltered.length <= 0 ? null : playerTeamFiltered[0]
     }
+
+    async findPlayerAll(ObjNamePosition, team){
+        // let playerTeamFiltered = null;
+        // if(!ObjNamePosition) return null;
+        // const playerTeam = await this.getPlayersByTeam(team);
+        // playerTeamFiltered = playerTeam.sheets.Players.filter(player => player[Object.keys(ObjNamePosition).toString()] === ObjNamePosition[Object.keys(ObjNamePosition).toString()])
+        // // console.log("Esto es: playerTeamFiltered");
+        // // console.log(playerTeamFiltered.length)
+        // return playerTeamFiltered.length <= 0 ? null : playerTeamFiltered[0]
+    }    
 
 }
 
--- eurob.test.js
+++ eurob.test.js
@@ -18,19 +18,56 @@ test('getTeams should return a array of 24 elements ', t => {
 
 test('getPlayersByTeam should return a team is Spain', async t => {
     const players = await EuroDB.getPlayersByTeam('spain')
-    console.log(players)
+    // console.log(players)
     t.is(typeof players, 'object');
 })
 
 test('getPlayersByTeam should return true if the Name of a one player is Iker Casillas', async t => {
     const players = await EuroDB.getPlayersByTeam('spain')
-    console.log(players.sheets.Players.find(player => player.name === 'Iker Casillas'));
+    // console.log(players.sheets.Players.find(player => player.name === 'Iker Casillas'));
     // t.not(players.sheets.Players.find(player => player.name === 'Iker Casillas'),null);
     t.is(!!players.sheets.Players.find(player => player.name === 'Iker Casillas'),true);
 })
 
-test('find should return true if finds the player in this team', async t => {
-    const playersExit = await EuroDB.findPlayer({name: "David de Gea"},'spain')
-    // console.log(players.sheets.Players.find(player => player.name === 'Iker Casillas'));
-    t.is(playersExit,true);
+test('getPlayersByTeam should return 404 if the teams name is not found', async t => {
+    const players = await EuroDB.getPlayersByTeam('norway')
+    // console.log(players)
+    t.is(players,404);
 })
+
+test('getPlayersByTeam should return null if the teams name is not found', async t => {
+    const players = await EuroDB.getPlayersByTeam(null)
+    // console.log(players)
+    t.is(players,404);
+})
+
+test('findPlayer should return the object named Iker Casillas from the spain team', async t => {
+    let player = await EuroDB.findPlayer({name: "Iker Casillas"},'spain')
+    // console.log(player);
+    t.is(player.name,'Iker Casillas');
+})
+
+test('findPlayer should return the object position Goalkeeper from the spain team', async t => {
+    let player = await EuroDB.findPlayer({position: "Goalkeeper"},'spain')
+    // console.log(player);
+    t.is(player.position,'Goalkeeper');
+})
+
+test('findPlayer should return null when not found this player', async t => {
+    let player = await EuroDB.findPlayer({name: "Iter Casillas"},'spain')
+    // console.log(player);
+    t.is(player,null);
+})
+
+test('findPlayer should return null when not found this position', async t => {
+    let player = await EuroDB.findPlayer({position: "medico"},'spain')
+    // console.log(player);
+    t.is(player,null);
+})
+
+test('findPlayer should return null if the object name o position is not informed', async t => {
+    let player = await EuroDB.findPlayer(null,'spain')
+    // console.log(player);
+    t.is(player,null);
+})
+
